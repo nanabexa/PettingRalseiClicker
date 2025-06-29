71feeea5c92a8a31ae8c9ab1ec43ee7da6f9985e
nanabexa <nietoviviana1075@gmail.com>
2025-06-29 01:14:40 -0500

    Tocamos todos los ralsei

---
+++ PettingRalseiClicker/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.ejemplo.pettingralseiclicker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.ejemplo.pettingralseiclicker to javafx.fxml;
+    exports com.ejemplo.pettingralseiclicker;
+}
